45a6db4ccb9e286f016850dfecf9d4be94e26c30
Gabriel Seet <malrhis@gmail.com>
2021-01-06 18:50:56 +0800

    fix validation for getpokemon1()

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     let pokemonAPI = {
       url: "https://pokeapi.co/api/v2/pokemon/" + pokemonNameInput,
       method: "GET",
-      timeout: 0
+      timeout: 0,
     };
     $.ajax(pokemonAPI).done(function (response) {
       pokemonPreview = response;
@@ -29,7 +29,7 @@ $(document).ready(function () {
     let allPokemonListCall = {
       url: "https://pokeapi.co/api/v2/pokemon?limit=1118",
       method: "GET",
-      timeout: 0
+      timeout: 0,
     };
     $.ajax(allPokemonListCall).done(function (response) {
       console.log(
@@ -52,63 +52,62 @@ $(document).ready(function () {
     e.preventDefault();
     localStorage.setItem("pokemon1", JSON.stringify(pokemonPreview));
     getPokemon1();
-  })
+  });
 
   // Adding Pokemon 2 to Party
   $("#save-button2").click(function (e) {
     e.preventDefault();
     localStorage.setItem("pokemon2", JSON.stringify(pokemonPreview));
     getPokemon2();
-  })
+  });
 
   // Adding Pokemon 3 to Party
   $("#save-button3").click(function (e) {
     e.preventDefault();
     localStorage.setItem("pokemon3", JSON.stringify(pokemonPreview));
     getPokemon3();
-  })
+  });
 
   // Adding Pokemon 4 to Party
   $("#save-button4").click(function (e) {
     e.preventDefault();
     localStorage.setItem("pokemon4", JSON.stringify(pokemonPreview));
     getPokemon4();
-  })
-  
+  });
+
   // Adding Pokemon 5 to Party
   $("#save-button5").click(function (e) {
     e.preventDefault();
     localStorage.setItem("pokemon5", JSON.stringify(pokemonPreview));
     getPokemon5();
-  })
+  });
 
   // Adding Pokemon 6 to Party
   $("#save-button6").click(function (e) {
     e.preventDefault();
     localStorage.setItem("pokemon6", JSON.stringify(pokemonPreview));
     getPokemon6();
-  })
-
+  });
 
   // Another function to extract from local storage and display in table
   function getPokemon1() {
-    pokemon1 = JSON.parse(localStorage.getItem("pokemon1"));
-    console.log(pokemon1)
-    let pokemon1NameInput = pokemon1.name
+    if (localStorage.getItem("pokemon1")) {
+      pokemon1 = JSON.parse(localStorage.getItem("pokemon1"));
+      console.log(pokemon1);
+      let pokemon1NameInput = pokemon1.name;
 
-    let pokemonAPI = {
-      url: "https://pokeapi.co/api/v2/pokemon/" + pokemon1NameInput,
-      method: "GET",
-      timeout: 0
-    };
-    $.ajax(pokemonAPI).done(function (response) {
-      pokemonPreview = response;
-      let pokemonSprite = pokemonPreview.sprites.front_default;
-      console.log("pokemonSprite", pokemonSprite);
+      let pokemonAPI = {
+        url: "https://pokeapi.co/api/v2/pokemon/" + pokemon1NameInput,
+        method: "GET",
+        timeout: 0,
+      };
+      $.ajax(pokemonAPI).done(function (response) {
+        pokemonPreview = response;
+        let pokemonSprite = pokemonPreview.sprites.front_default;
+        console.log("pokemonSprite", pokemonSprite);
 
-      $("img.pokemon1").attr("src", pokemonSprite);
-    })
+        $("img.pokemon1").attr("src", pokemonSprite);
+      });
+    }
   }
 });
-
-
