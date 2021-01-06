ddd87f4303b9b7ad50e4139134ca25bae708ff95
Malrhis <malrhis@gmail.com>
2021-01-07 02:05:38 +0800

    added new API call for pokemon genus and type

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // 1: Check that DOM is ready
 $(document).ready(function () {
-  // pull saved pokemon
+  // pull saved party pokemon
   getPokemon(1);
   getPokemon(2);
   getPokemon(3);
@@ -10,11 +10,32 @@ $(document).ready(function () {
 
   // declare variables
   let pokemonPreview;
-  let partyPokemon = [];
+  let pokemonPreviewSpecies;
+  let typeURL = {
+    bug: "https://www.serebii.net/pokedex-bw/type/bug.gif",
+    dark:"https://www.serebii.net/pokedex-bw/type/dark.gif",
+    dragon:"https://www.serebii.net/pokedex-bw/type/dragon.gif",
+    electric:"https://www.serebii.net/pokedex-bw/type/electric.gif",
+    fairy:"https://www.serebii.net/pokedex-bw/type/fairy.gif",
+    fighting:"https://www.serebii.net/pokedex-bw/type/fighting.gif",
+    fire:"https://www.serebii.net/pokedex-bw/type/fire.gif",
+    flying:"https://www.serebii.net/pokedex-bw/type/flying.gif",
+    ghost:"https://www.serebii.net/pokedex-bw/type/ghost.gif",
+    grass:"https://www.serebii.net/pokedex-bw/type/grass.gif",
+    ground:"https://www.serebii.net/pokedex-bw/type/ground.gif",
+    ice:"https://www.serebii.net/pokedex-bw/type/ice.gif",
+    normal:"https://www.serebii.net/pokedex-bw/type/normal.gif",
+    poison:"https://www.serebii.net/pokedex-bw/type/poison.gif",
+    psychic:"https://www.serebii.net/pokedex-bw/type/psychic.gif",
+    rock:"https://www.serebii.net/pokedex-bw/type/rock.gif",
+    steel:"https://www.serebii.net/pokedex-bw/type/steel.gif",
+    water:"https://www.serebii.net/pokedex-bw/type/water.gif",
+  }
 
   $("#form-pokemon-entry").submit(function (e) {
     e.preventDefault();
     let pokemonNameInput = $("#text-name").val();
+    
     let pokemonAPI = {
       url: "https://pokeapi.co/api/v2/pokemon/" + pokemonNameInput,
       method: "GET",
@@ -22,11 +43,29 @@ $(document).ready(function () {
     };
     $.ajax(pokemonAPI).done(function (response) {
       pokemonPreview = response;
-      let pokemonSprite = pokemonPreview.sprites.front_default;
-      console.log("pokemonSprite", pokemonSprite);
+      console.log(pokemonPreview.sprites.front_default);
+      console.log(typeURL[pokemonPreview.types[0].type.name]);
 
-      $("img.pokemonPreview").attr("src", pokemonSprite);
+      $("img.pokemonPreview").attr("src", pokemonPreview.sprites.front_default);
+      $(".pokemon-preview-name").html(pokemonPreview.name.charAt(0).toUpperCase() + pokemonPreview.name.slice(1));
+      $("img.pokemon-preview-type1").attr("src", typeURL[pokemonPreview.types[0].type.name]);
+      if (pokemonPreview.types[1]) {
+        $("img.pokemon-preview-type2").attr("src", typeURL[pokemonPreview.types[1].type.name]);
+      } else {
+        $("img.pokemon-preview-type2").attr("src", "")
+      }
     });
+
+    let pokemonspeciesAPI = {
+      url: "https://pokeapi.co/api/v2/pokemon-species/" + pokemonNameInput,
+      method: "GET",
+      timeout: 0,
+    };
+    $.ajax(pokemonspeciesAPI).done(function (response) {
+      pokemonPreviewSpecies = response;
+      console.log(pokemonPreviewSpecies.genera[7].genus);
+      $(".pokemon-preview-genus").html(pokemonPreviewSpecies.genera[7].genus.charAt(0).toUpperCase() + pokemonPreviewSpecies.genera[7].genus.slice(1));
+    })
   });
 
   // Autocomplete search bar
